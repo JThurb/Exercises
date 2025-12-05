34018f373de1900b646ba3fd240e51c1f4efe72e
Janne Turpeinen <117199626+JThurb@users.noreply.github.com>
2025-12-05 14:15:09 +0200

    Update restapi-get-juna2.py

--- restapi-get-juna2.py
+++ restapi-get-juna2.py
@@ -1,3 +1,6 @@
+# Ohjelma hakee Digitrafficin datasta, annetun päivämäärän kaikki junat, jotka on määritetty tyypiltään IC eli Inter City ja kategorialtaan Long Distance. 
+# Junien Lähtö- ja päätepysäkit tulostetaan junanumeron kanssa. 
+
 import requests
 
 päivä = "2025-12-01"
@@ -6,28 +9,31 @@ url = f"https://rata.digitraffic.fi/api/v1/trains/{päivä}"
 response = requests.get(url)
 
 
-if response.status_code == 200:
+if response.status_code == 200:  # Jos GET-pyyntö on onnistunut
     data = response.json()
-
+    
+    # Käydään saatu data läpi ja tallennetaan muutujiin
     for train in data:
         traintype = train.get("trainType")
         traincategory = train.get("trainCategory")
         trainnumber = train.get("trainNumber")
-
+        
+        # Määritetään tyyppi ja kategoria ja muodostetaan lista nimeltä rows
         if traintype == "IC" and traincategory == "Long-distance":
             rows = train.get("timeTableRows", [])
             
-            # lähtöasema
+            # Etsitään lähtöasema
             lähtö_rivi = next(r for r in rows if r["type"] == "DEPARTURE")
             lähtöasema = lähtö_rivi["stationShortCode"]
             
-            # määränpääasema
+            # Etsitään määränpääasema
             määränpää_row = [r for r in rows if r["type"] == "ARRIVAL"][-1]
-            määränpää_station = määränpää_row["stationShortCode"]
+            määränpää_asema = määränpää_row["stationShortCode"]
 
-            print(f"Juna: {trainnumber} {lähtöasema} -> {määränpää_station}")
+            print(f"Juna: {trainnumber} {lähtöasema} -> {määränpää_asema}")
 
 else:
-    print(f"GET response faulty. Code: {response.status_code}")
+    print(f"Tapahtui virhe GET-pyynnössä. Koodi: {response.status_code}")
+
 
 
