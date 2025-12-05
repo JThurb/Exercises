1b1c7548d890fe19740337bb36f6adc9be855e03
Janne Turpeinen <117199626+JThurb@users.noreply.github.com>
2025-12-05 13:54:32 +0200

    Update POST_and_PUT.py

--- POST_and_PUT.py
+++ POST_and_PUT.py
@@ -25,10 +25,10 @@ else:
 update_product = {'id': product_id,
             'name': 'Playstation 5 pro',    # name changed
             'data': {'Generation': 'pro',  # slim to pro
-                     'Price': '949.99',    # Price updated too
+                     'Price': '949.99',    # Price updated
                      'Capacity': '1 TB'}}
 
-put_url = f"{api_url}/{product_id}"         # forms an put url with combining api_url and product id with /
+put_url = f"{api_url}/{product_id}"         # forms the PUT url with combining api_url and product id with /
 
 payload = json.dumps(update_product)           # data to be sended to server formated to json
 headers = {"content-type": "application/json"} # tells to server that data is in json format
@@ -38,4 +38,5 @@ put_response = requests.put(put_url, data=payload, headers=headers)
 if put_response.status_code in (200, 204):
     print("Product updated successfully")
 else:
+
     print("Error updating product", put_response.status_code, put_response.text)
